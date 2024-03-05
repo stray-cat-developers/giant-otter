a9211c86089797e0b9b47f4c5b0cf8b030237ac7
stan.marsh <stan.marsh@sk.com>
2024-03-05 12:11:56 +0900

    fixed: test code

--- src/test/kotlin/io/mustelidae/grantotter/config/FlowTestSupport.kt
+++ src/test/kotlin/io/mustelidae/grantotter/config/FlowTestSupport.kt
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -14,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc
 
 @ActiveProfiles("embedded")
 @ExtendWith(SpringExtension::class)
-@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator::class)
 @SpringBootTest(classes = [GrantOtterApplication::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = [DefaultEmbeddedMongo::class])
 @AutoConfigureMockMvc
